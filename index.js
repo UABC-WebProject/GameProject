ab13f65315a6d112aeecdf08878b2cffd666058b
Diego <diego.caudillo@uabc.edu.mx>
2021-01-06 22:56:14 -0800

    Add 404 Not found land page

--- index.js
+++ index.js
@@ -516,3 +516,7 @@ app.post('/deleteGameOfMyList', (req, res)=>{
 app.listen(3000, ()=>{
     console.log("Server started on port 3000");
 });
+
+app.use((req, res, next)=>{
+    res.status(400).render(__dirname+"/views/failure.ejs", {landPage: "404", userLogged: req.user, authenticated:true});
+})
